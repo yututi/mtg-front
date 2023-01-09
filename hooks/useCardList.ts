c72c8cf0310ebf13b967131dcb3f78df59d77cac
yututi <yututi@users.noreply.github.com>
2023-01-09 07:12:37 +0000

    とりあえず表示するとこまで

---
+++ hooks/useCardList.ts
@@ -0,0 +1,39 @@
+import useSWR from "swr"
+import { CardApi } from "../lib/gen/axios"
+
+const api = new CardApi()
+
+type CardListParams = {
+  page: number
+  name?: string
+  color?: string[]
+  cost?: string
+  types?: string[]
+  rarity?: string[]
+  setCode?: string[]
+}
+
+const useCardList = (param: CardListParams) => {
+
+  console.log(typeof window === undefined)
+
+  const result = useSWR(JSON.stringify(param), () => {
+
+    return api.getList(
+      param.page,
+      param.name,
+      param.color as Array<'B' | 'W' | 'U' | 'G' | 'R' | 'NONE'>,
+      param.cost,
+      param.types,
+      param.rarity,
+      param.setCode)
+  }, {
+    revalidateIfStale: false,
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false
+  })
+
+  return result
+}
+
+export default useCardList;
